d1ff4ed77c638ecf852ae78b3c0a2c77c5149e8f
Endre Toducz <endre.toducz@electrolux.com>
2023-04-07 16:41:58 +0300

    yarn lint

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,16 @@
-import { Toolbar } from "@material-ui/core";
-import Button from "@material-ui/core/Button";
-import { initializeApp } from "firebase/app";
-import { UserCredential, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { Toolbar } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBSBAq8JTqvZbbF5mKc20tM5o9IvpqWFOY",
-  authDomain: "social-game-d2747.firebaseapp.com",
-  projectId: "social-game-d2747",
-  storageBucket: "social-game-d2747.appspot.com",
-  messagingSenderId: "135347244914",
-  appId: "1:135347244914:web:b8ee200b532cd2a77157eb",
-  measurementId: "G-3K4YYXMFTS"
+  apiKey: 'AIzaSyBSBAq8JTqvZbbF5mKc20tM5o9IvpqWFOY',
+  authDomain: 'social-game-d2747.firebaseapp.com',
+  projectId: 'social-game-d2747',
+  storageBucket: 'social-game-d2747.appspot.com',
+  messagingSenderId: '135347244914',
+  appId: '1:135347244914:web:b8ee200b532cd2a77157eb',
+  measurementId: 'G-3K4YYXMFTS'
 };
 
 const app = initializeApp(firebaseConfig);
@@ -19,18 +19,11 @@ const auth = getAuth(app);
 
 function App() {
 
-  console.log(auth);
-  const email = "toducz.endre@gmail.com";
-  const password = "password";
+  const email = 'toducz.endre@gmail.com';
+  const password = 'passwor';
 
-  const logInWithEmailAndPassword = async (email: string, password: string) => {
-    try {
-      let response: UserCredential = await signInWithEmailAndPassword(auth, email, password);
-      console.log(response);
-    } catch (err: any) {
-      console.error(err);
-      alert(err.message);
-    }
+  const logInWithEmailAndPassword = async (currentEmail: string, currentPassword: string) => {
+      await signInWithEmailAndPassword(auth, currentEmail, currentPassword);
   };
 
   return (
