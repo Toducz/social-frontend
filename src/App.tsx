964669bb5f03812dcd91db977437fe9d55280f87
Endre Toducz <endre.toducz@electrolux.com>
2023-04-11 12:49:06 +0300

    reconfig project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+  const alma = 'asd';
+
+  return <>Endi</>;
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
-import { render } from 'react-dom';
-import App from './components/App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const container = document.getElementById('root');
-render(<App />, container);
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
